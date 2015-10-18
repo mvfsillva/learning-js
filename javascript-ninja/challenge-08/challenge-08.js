1768ebfa9dabcdb3b121b67c14d71152ce709f8f
mvfsilva <marcusvinicius30798@gmail.com>
2015-10-17 22:42:29 -0300

    rename folder

--- javascript-ninja/challenge-08/challenge-08.js
+++ javascript-ninja/challenge-08/challenge-08.js
@@ -3,14 +3,16 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum(a,b){
+    console.log(return a + b);
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+sum.calculateSum(1,1);
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
