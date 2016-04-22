4e08ec6cb572dc6917f2601ccdf355deb61971a9
mvfsilva <marcusvinicius30798@gmail.com>
2016-04-21 23:00:11 -0300

    Create new method

--- javascript-ninja/challenge-29/challenge-29.js
+++ javascript-ninja/challenge-29/challenge-29.js
@@ -36,7 +36,7 @@
   que será nomeado de "app".
   */
 
-  function app(){
+  var app = (function controller(){
     return{
       init: function(){
         this.companyInfo();
@@ -49,7 +49,34 @@
 
       handleSubmit: function handleSubmit(e){
         e.preventDefault();
-        console.log('submit');
+        var $tableCar = $('[data-js="info-car"]').get();
+        $tableCar.appendChild(app.createNewCar());
+      },
+
+      createNewCar: function createNewCar() {
+        var $fragment = document.createDocumentFragment();
+        var $tr = document.createElement('tr');
+        var $tdImage = document.createElement('td');
+        var $image = document.createElement('img');
+        var $tdBrand = document.createElement('td');
+        var $tdYear  = document.createElement('td');
+        var $tdPlate = document.createElement('td');
+        var $tdColor = document.createElement('td');
+
+        $image.src = $('[data-js="image"]').get().value;
+        $tdImage.appendChild($image);
+        $tdBrand.textContent = $('[data-js="brand-model"]').get().value;
+        $tdYear.textContent = $('[data-js="year"]').get().value;
+        $tdPlate.textContent = $('[data-js="plate"]').get().value;
+        $tdColor.textContent = $('[data-js="color"]').get().value;
+
+        $tr.appendChild($tdImage);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
+
+        return $fragment.appendChild($tr);
       },
 
       companyInfo: function companyInfo() {
@@ -60,7 +87,7 @@
       },
 
       getCompanyInfo: function getCompanyInfo() {
-        if(!app().isReady.call(this))
+        if(!app.isReady.call(this))
           return;
 
         var data = JSON.parse(this.responseText);
@@ -73,8 +100,9 @@
       isReady: function isReady() {
         return this.readyState === 4 && this.status === 200;
       }
-
     }
-  }
-  app().init();
+  })();
+
+  app.init();
+
 })(window.DOM);
