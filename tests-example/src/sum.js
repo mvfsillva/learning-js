1c17961ae13d8aac85cca7b3e0218a95d79a8a8d
mvfsilva <marcusvinicius30798@gmail.com>
2016-04-30 23:43:25 -0300

    Refactor module test

--- tests-example/src/sum.js
+++ tests-example/src/sum.js
@@ -1,9 +1,18 @@
 'use strict';
 
 function sum(num1, num2) {
-  if (!num1 || !num2)
+  if (isEmpty(num1) || isEmpty(num2)){
     return new Error('Passe dois números por parâmetro');
+  }
+
+  if(isNaN(num1) || isNaN(num2)){
+    return new Error('Os parâmetros devem ser números');
+  }
   return num1 + num2;
+}
+
+function isEmpty(arg) {
+  return !arg;
 }
 
 module.exports = sum;
