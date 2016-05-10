b3c7fadc9b56a882705556a898ff8f10fdaa66c4
mvfsilva <marcusvinicius30798@gmail.com>
2016-05-09 22:31:01 -0300

    Create class controller

--- ES2015/src/controller.js
+++ ES2015/src/controller.js
@@ -1,2 +1,23 @@
 import axios from 'axios';
 import Model from './model';
+
+const host = 'http://localhost:4000';
+
+class Controler{
+  constructor(){
+    this.render();
+  };
+
+  parseHtml(str){
+    let domparser = new DOMParser();
+    let doc = domparser.parseFromString(srt, 'text/html');
+    let  docfrag = document.createDocumentFragment();
+    doc.addEventListener('click', function(){
+      console.log('teste');
+    }, false);
+    docfrag.appendChild(doc.documentElement);
+    return docfrag;
+  }
+}
+
+export default Controller;
