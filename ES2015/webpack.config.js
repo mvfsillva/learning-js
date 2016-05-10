0d9cb272e63e5e06a3eb24eafbdd679098733f6b
mvfsilva <marcusvinicius30798@gmail.com>
2016-05-09 22:42:50 -0300

    Update webpack

--- ES2015/webpack.config.js
+++ ES2015/webpack.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: 'build/',
-        filename: 'app.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ["babel"]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-        historyApiFallback: true
-    }
+  entry: './src/app.js',
+  output: {
+    path: 'build/',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["babel"]
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true
+  }
 };
