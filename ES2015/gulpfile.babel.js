f7fdcbdc68ee1c84ec032929b90d8ae1dfa61f7f
mvfsilva <marcusvinicius30798@gmail.com>
2016-05-09 22:43:34 -0300

    Initial work

--- ES2015/gulpfile.babel.js
+++ ES2015/gulpfile.babel.js
@@ -1,1 +1,12 @@
-import gulp from 'gulp';
+let gulp = require('gulp');
+let runSequence = require('run-sequence');
+let webpack = require('webpack-stream');
+let watch = require('gulp-watch');
+let browserSync = require('browser-sync').create();
+let historyApiFallback = require('connect-history-api-fallback');
+let reload = browserSync.reload;
+
+function handleError(error) {
+  console.log(err.toString());
+  this.emit('end');
+}
