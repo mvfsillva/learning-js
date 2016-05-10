ed9fff9c3e80c7d7c36148132dbfdd2f19c188ca
mvfsilva <marcusvinicius30798@gmail.com>
2016-05-09 22:36:05 -0300

    Create class App

--- ES2015/src/app.js
+++ ES2015/src/app.js
@@ -1,1 +1,9 @@
-import Controller from './controller';
+import WebComponents from 'webcomponents.js';
+import PostsIndex from './components/posts_index';
+
+class App{
+  constructor(){
+  }
+}
+
+const app = new App();
