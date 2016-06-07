470b739d502b3eb158a6b9cf0b137d5c6d24345d
mvfsilva <marcusvinicius30798@gmail.com>
2016-06-07 18:00:47 -0300

    Create model Negotiation

---
+++ ES2015/negotiation/client/js/app/models/Negotiation.js
@@ -0,0 +1,24 @@
+class Negotiation {
+    constructor(date, qtd, val) {
+        this._date = new Date(data.getTime());
+        this._qtd = 1;
+        this._val = 0.0;
+        Object.freeze(this); // makes all read only
+    }
+
+    get volume() {
+        return this.qtd * this.val;
+    }
+
+    get date() {
+        return new Date(this._date.getTime());
+    }
+
+    get qtd() {
+        return this._qtd;
+    }
+
+    get val() {
+        this._val;
+    }
+}
