156f374d4145a53d583c268f69d3b5a379c2e8a8
mvfsilva <marcusvinicius30798@gmail.com>
2016-06-15 14:05:21 -0300

    Refactored

--- ES2015/negotiation/client/js/app/controllers/NegotiationController.js
+++ ES2015/negotiation/client/js/app/controllers/NegotiationController.js
@@ -2,33 +2,28 @@ class NegotiationController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
+        this._form = $('[data-js="form"]');
         this._inputDate = $('[data-js="data"]');
         this._inputQtd = $('[data-js="quantidade"]');
         this._inputVal = $('[data-js="valor"]');
-        this._listNegotiations = new ListNegotiation();
 
-        this._negotiationsView = new NegotiationView($('[data-js="negociacoesView"]'));
-        this._listNegotiations = new ListNegotiation(model =>
-            this._negotiationsView.update(model)
+        this._listNegotiations = new Bind(
+            new ListNegotiation(), new NegotiationView($('[data-js="negotiations-view"]')), 'insert', 'clear'
         );
 
-        this._message = new Message();
-        this._messageView = new MessageView($('[data-js="messageView"]'));
-        this._messageView.update(this._message);
+        this._message = new Bind(new Message(), new MessageView($('[data-js="message-view"]')), 'text');
     }
 
     insert(event) {
         event.preventDefault();
         this._listNegotiations.insert(this._createNegotiation());
-        this._negotiationsView.update(this._listNegotiations);
         this._message.text = 'Negociação adicionada com sucesso!';
-        this._isClearForm();
+        this._clearForm();
     }
 
     delete() {
         this._listNegotiations.clear();
         this._message.text('Negociação apagada com sucesso!');
-        this._messageView.update(this._message);
     }
 
     _createNegotiation() {
@@ -39,8 +34,8 @@ class NegotiationController {
         );
     }
 
-    _isClearForm() {
-        $('[data-js="form"]').reset();
+    _clearForm() {
+        this._form.reset();
         this._inputDate.focus();
     }
 }
--- ES2015/negotiation/client/js/app/services/ProxyFactory.js
+++ ES2015/negotiation/client/js/app/services/ProxyFactory.js
@@ -1,9 +1,9 @@
 class ProxyFactory {
     static create(obj, props, action) {
         return new Proxy(obj, {
-            get(target, prop, receiver){
-                if(props.includes(prop) && ProxyFactory._isFunction(target[prop])){
-                    return function () {
+            get(target, prop, receiver) {
+                if (props.includes(prop) && ProxyFactory._isFunction(target[prop])) {
+                    return function() {
                         Reflect.apply(target[prop], target, arguments);
                         return action(target);
                     }
@@ -11,17 +11,17 @@ class ProxyFactory {
                 return Reflect.get(target, prop, receiver);
             },
 
-            set (target, prop, value, receiver) {
-                if(props.includes(prop)){
+            set(target, prop, value, receiver) {
+                if (props.includes(prop)) {
                     target[prop] = value;
                     action(target);
                 }
                 return Reflect.set(target, prop, value, receiver);
             }
-        })
+        });
     }
 
-    static _isFunction(func){
+    static _isFunction(func) {
         return typeof(func) === 'function';
     }
 }
